9b2c1e2fcbe7e7e754413655891d4b1c3184ef07
이중권 <2jungg@hankookn.com>
2026-02-05 10:33:35 +0900

    feat: add build.rs

---
+++ src/build.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    let profile = env::var("PROFILE").unwrap();
+    
+    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dll_source = PathBuf::from(project_dir).join("lib").join("onnxruntime.dll");
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let target_dir = PathBuf::from(out_dir)
+        .ancestors()
+        .nth(3) 
+        .unwrap()
+        .to_path_buf();
+
+    let dest_path = target_dir.join("onnxruntime.dll");
+    
+    if dll_source.exists() {
+        fs::copy(&dll_source, &dest_path).expect("DLL 복사 실패!");
+        println!("cargo:warning=Copied ONNX Runtime DLL from {:?} to {:?}", dll_source, dest_path);
+    } else {
+        println!("cargo:warning=ONNX Runtime DLL not found at {:?}", dll_source);
+    }
+
+    println!("cargo:rerun-if-changed=lib/onnxruntime.dll");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 mod convert;
 mod func_utils;
 
+use std:: {env, path::PathBuf};
 use convert::{TextProcessor};
 use func_utils::{cosine_similarity};
 
+fn get_dll_path(dll_name: &str) -> PathBuf {
+    let mut exe_path = env::current_exe().expect("Failed to get exe path");
+    exe_path.pop();
+    exe_path.push(dll_name);
+    exe_path
+}
 fn main() -> anyhow::Result<()> {
-    let dylib_path = "C:/Users/hanta/Documents/projects/MyVectorDB/lib/onnxruntime.dll";
+    let dylib_path = get_dll_path("onnxruntime.dll");
+    println!("Attempting to load ONNX Runtime DLL from: {:?}", dylib_path);
     ort::init_from(dylib_path)?.commit();
 
     let mut processor = TextProcessor::new("bge-m3/onnx/tokenizer.json", "bge-m3/onnx/model.onnx")?;
